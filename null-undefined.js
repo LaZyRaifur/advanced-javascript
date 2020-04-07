ea310b386a9c1994b5ae69247dd1f46955b5b1e5
Raifurrahim <raifurrahimcse13@gmail.com>
2020-04-08 02:44:56 +0600

    null-undefined for interview

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+
+let pakhi;
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+
+
+const premik = {name:"smart dude",phone:458721};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,10];
+console.log(ages[11]);
+
