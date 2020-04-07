d4adbb1d830a69c35a5a2a5a02910ce8250a292b
Raifurrahim <raifurrahimcse13@gmail.com>
2020-04-08 00:22:17 +0600

    update

--- true-fales.js
+++ true-fales.js
@@ -1,5 +1,16 @@
-const age = 4;
-if(age){
+//false
+//0
+//""
+//NaN
+//null
+
+//truth
+//'0', ' ',[]
+
+
+//const age = 4;
+let name = 'false';
+if(name){
     console.log("condition is true");
 }
 else{
